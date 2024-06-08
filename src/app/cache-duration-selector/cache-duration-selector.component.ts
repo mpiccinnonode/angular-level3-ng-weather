ceca4e814395789e558bfc4f4e4faed9ac0e33e9
MaxPiccinno <m.piccinno@node.coop>
2024-06-08 12:47:29 +0200

    duration form template

--- src/app/cache-duration-selector/cache-duration-selector.component.ts
+++ src/app/cache-duration-selector/cache-duration-selector.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CacheService } from '../core/services/cache.service';
 import { DurationForm } from './models/duration-form.model';
 import { isIntegerValidator } from './validators/is-integer.validator';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
     selector: 'app-cache-duration-selector',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule, NgTemplateOutlet],
     templateUrl: './cache-duration-selector.component.html',
     styleUrl: './cache-duration-selector.component.css',
 })
-export class CacheDurationSelectorComponent {
+export class CacheDurationSelectorComponent implements OnInit {
     durationForm: FormGroup<DurationForm>;
 
     constructor(
         private fb: FormBuilder,
         private cacheService: CacheService,
     ) {}
+
+    ngOnInit(): void {
+        this._initForm();
+    }
 
     private _initForm(): void {
         this.durationForm = this.fb.group<DurationForm>({
